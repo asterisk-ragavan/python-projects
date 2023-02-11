5bf3fe5f284e627190d5c23a20b274eda193eea4
Sakthi Ragavan <75389172+asterisk-ragavan@users.noreply.github.com>
2023-02-11 17:04:26 +0530

    update and fix

--- palan_project/main.py
+++ palan_project/main.py
@@ -1,27 +1,17 @@
-import socket
-
-HOST = 'data.pr4e.org'
-PORT = 80
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect((HOST, PORT))
-mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
-
-count = 0
-picture = b""
-
-while True:
-    data = mysock.recv(5120)
-    if len(data) < 1: break
-    count = count + len(data)
-    print(len(data), count)
-    picture = picture + data
-mysock.close()
-
-pos = picture.find(b"\r\n\r\n")
-print('Header length', pos)
-print(picture[:pos].decode())
-
-picture = picture[pos + 4:]
-fhand = open("stuff.jpg", "wb")
-fhand.write(picture)
-fhand.close()
+inp = input()
+d=0
+de = len(inp)-2
+temp = []
+ver = []
+for i in range(de):
+    temp = 0
+    for j in inp:
+        if(j!=de+2):
+            ver.append(int(j))
+            de= de-1
+            d=d+1
+    temp.append(ver.reverse())
+    if(j==de):
+        temp.append(int(j))
+    inp=temp
+print(temp[len(temp)-1])
